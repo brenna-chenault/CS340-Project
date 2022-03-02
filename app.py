f21955f5e2809e9b7fcc4e60ff737b27bbae797d
mliu08 <liumar@oregonstate.edu>
2022-03-02 13:53:51 -0800

    ordered_products has C/R functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, redirect
+from flask import Flask, render_template, redirect
 import os
 from flask_mysqldb import MySQL
 from flask import request
@@ -85,7 +85,6 @@ def orders():
         # Render the Orders page with the fetched data
         return render_template("orders.j2", order_data = orders_table, order_nums = order_nums)
 
-
 # Products
 @app.route('/products.html', methods=["POST", "GET"])
 def products():
@@ -127,7 +126,45 @@ def products():
         # Render the Products page with the fetched data
         return render_template("products.j2", prod_data = prod_table, prod_nums = prod_nums)
 
+# Ordered_Products
+@app.route('/ordered_products.html', methods=["POST", "GET"])
+def ordered_products():
+    """ 
+    Route for the Ordered_Products page. Has Create, Read, and Delete functionality.
+    """
+    # Add an Ordered_Product to the database 
+    if request.method == "POST":                    
+        if request.form.get("insert_order_product_submit"):
+            order_id = request.form["order_id"]
+            product_id = request.form["product_id"]
+            number_ordered = request.form["number_ordered"]
+            ordered_cost = request.form["ordered_cost"]
 
+            # No NULLable attributes
+            query = "INSERT INTO Ordered_Products (order_id, product_id, number_ordered, ordered_cost) VALUES (%s, %s, %s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (order_id, product_id, number_ordered, ordered_cost))
+            mysql.connection.commit()
+
+            # redirect back to people page
+            return redirect("/ordered_products.html")
+
+    # Get data to display in table and delete dropdown
+    if request.method == "GET":
+        # Query to populate table
+        query_table = "SELECT * FROM Ordered_Products"
+        cur = mysql.connection.cursor()
+        cur.execute(query_table)
+        order_prod_table = cur.fetchall()
+    
+        # Query to fill delete dropdown with product_id
+        query_id_dropdown = "SELECT order_product_id FROM Ordered_Products"
+        cur = mysql.connection.cursor()
+        cur.execute(query_id_dropdown)
+        order_prod_nums = cur.fetchall()
+
+        # Render the Products page with the fetched data
+        return render_template("ordered_products.j2", order_prod_data = order_prod_table, order_prod_nums = order_prod_nums)
 
 
 

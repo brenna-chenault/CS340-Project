b309989f8e7842c6f6d78366b5aa01d413c3684a
Brenna Chenault <chenaulb@flip1.engr.oregonstate.edu>
2022-03-02 20:09:46 -0800

    added CREATE and READ to manufacturers, warehouses, and shippers

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 # database connection - from template
 app.config["MYSQL_HOST"] = "classmysql.engr.oregonstate.edu"
-app.config["MYSQL_USER"] = "cs340_liumar"
-app.config["MYSQL_PASSWORD"] = "3794"
-app.config["MYSQL_DB"] = "cs340_liumar"
+app.config["MYSQL_USER"] = "cs340_chenaulb"
+app.config["MYSQL_PASSWORD"] = "7566"
+app.config["MYSQL_DB"] = "cs340_chenaulb"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
@@ -19,6 +19,7 @@ mysql = MySQL(app)
 
 # Homepage
 @app.route('/index.html')
+@app.route('/')
 def root():
     return render_template("/index.j2")
 
@@ -166,10 +167,131 @@ def ordered_products():
         # Render the Products page with the fetched data
         return render_template("ordered_products.j2", order_prod_data = order_prod_table, order_prod_nums = order_prod_nums)
 
+# Manufacturers
+@app.route('/manufacturers.html', methods=["POST", "GET"])
+def manufacturers():
+    """ 
+    Route for the Manufacturers page. Has Create and Read functionality.
+    """
+    # Add a Manufacturer to the database 
+    if request.method == "POST":                    
+        if request.form.get("insert_manufacturer_submit"):
+            manufacturer_name = request.form["manufacturer_name"]
+            email = request.form["email"]
+            phone_number = request.form["phone_number"]
+            street_address = request.form["street_address"]
+            city = request.form["city"]
+            state = request.form["state"]
+            zip = request.form["zip"]
 
+            # No NULLable attributes
+            query = "INSERT INTO Manufacturers (manufacturer_name, email, phone_number, street_address, city, state, zip) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (manufacturer_name, email, phone_number, street_address, city, state, zip))
+            mysql.connection.commit()
+
+            # redirect back to people page
+            return redirect("/manufacturers.html")
+
+    # Get data to display in table
+    if request.method == "GET":
+        # Query to populate table
+        query_table = "SELECT * FROM Manufacturers"
+        cur = mysql.connection.cursor()
+        cur.execute(query_table)
+        man_table = cur.fetchall()
+
+        # Render the Products page with the fetched data
+        return render_template("manufacturers.j2", man_data = man_table)
+
+# Shippers
+@app.route('/shippers.html', methods=["POST", "GET"])
+def shippers():
+    """ 
+    Route for the Shippers page. Has Create and Read functionality.
+    """
+    # Add a Shippers to the database 
+    if request.method == "POST":                    
+        if request.form.get("insert_shipper_submit"):
+            shipper_name = request.form["shipper_name"]
+            shipper_account_num = request.form["shipper_account_num"]
+            shipper_contact_name = request.form["shipper_contact_name"]
+            shipper_contact_email = request.form["shipper_contact_email"]
+
+            # No NULLable attributes
+            query = "INSERT INTO Shippers (shipper_name, shipper_account_num, shipper_contact_name, shipper_contact_email) VALUES (%s, %s, %s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (shipper_name, shipper_account_num, shipper_contact_name, shipper_contact_email))
+            mysql.connection.commit()
+
+            # redirect back to people page
+            return redirect("/shippers.html")
+
+    # Get data to display in table
+    if request.method == "GET":
+        # Query to populate table
+        query_table = "SELECT * FROM Shippers"
+        cur = mysql.connection.cursor()
+        cur.execute(query_table)
+        ship_table = cur.fetchall()
+
+        # Render the Products page with the fetched data
+        return render_template("shippers.j2", ship_data = ship_table)
+
+# Warehouses
+@app.route('/warehouses.html', methods=["POST", "GET"])
+def warehouses():
+    """ 
+    Route for the Warehouses page. Has Create and Read functionality.
+    """
+
+    # pull parameter from query string in GET
+    state_option = request.args.get('state_option')
+
+    # Add a Warehouse to the database
+    if request.method == "POST":                    
+        if request.form.get("insert_warehouse_submit"):
+            street_address = request.form["street_address"]
+            city = request.form["city"]
+            state = request.form["state"]
+            zip = request.form["zip"]
+
+            # No NULLable attributes
+            query = "INSERT INTO Warehouses (street_address, city, state, zip) VALUES (%s, %s, %s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (street_address, city, state, zip))
+            mysql.connection.commit()
+
+            # redirect back to people page
+            return redirect("/warehouses.html")
+
+    # Get data to display in table
+    if request.method == "GET":
+            
+        # Query to populate table with all warehouse data
+        if state_option is None or state_option == "Show+All+States":
+            query_table = "SELECT * FROM Warehouses"
+            cur = mysql.connection.cursor()
+            cur.execute(query_table)
+            ware_table = cur.fetchall()
+
+        # Query to populate table with selected state
+        else:
+            query_table = "SELECT * FROM Warehouses WHERE state=%s"
+            cur = mysql.connection.cursor()
+            cur.execute(query_table, [state_option])
+            ware_table = cur.fetchall()
+
+        # Query to fill filter select box with state
+        query_id_dropdown = "SELECT state FROM Warehouses"
+        cur = mysql.connection.cursor()
+        cur.execute(query_id_dropdown)
+        state_list = cur.fetchall()
+
+        # Render the Products page with the fetched data
+        return render_template("warehouses.j2", ware_data = ware_table, state_data = state_list)
 
 # Listener
-
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 9112))
+    port = int(os.environ.get('PORT', 3568))
     app.run(port=port, debug=True)
